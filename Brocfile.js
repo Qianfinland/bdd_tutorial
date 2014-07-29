2a1fb9c122c3a37f90d5b60535e29025a3fc450f
Qianfinland <qianfinland@gmail.com>
2014-07-29 15:38:52 +0300

    first commit

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,90 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+//add code here
+// Mocha support
+var pickFiles = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
+//comment below
+// var app = new EmberApp();
+var app = new EmberApp({
+  name: require('./package.json').name,
+
+  minifyCSS: {
+    enabled: true,
+    options: {}
+  },
+
+  getEnvJSON: require('./config/environment')
+});
+
+// Mocha support: JSHint tests are current QUnit and fail to run
+app.hinting = false;
+//
+
+// Use this to add additional libraries to the generated output files.
+app.import('vendor/ember-data/ember-data.js');
+
+// If the library that you are including contains AMD or ES6 modules that
+// you would like to import into your application please specify an
+// object with the list of modules as keys along with the exports of each
+// module as its value.
+app.import('vendor/ic-ajax/dist/named-amd/main.js', {
+  'ic-ajax': [
+    'default',
+    'defineFixture',
+    'lookupFixture',
+    'raw',
+    'request',
+  ]
+});
+
+// more Mocha support
+var mochaFiles = pickFiles('vendor', {
+    srcDir: '/mocha',
+    files: [
+      'mocha.css', 'mocha.js'
+    ],
+    destDir: '/assets/'
+  });
+
+var chaiFiles = pickFiles('vendor', {
+    srcDir: '/chai',
+    files: [
+      'chai.js'
+    ],
+    destDir: '/assets/'
+  });
+
+var sinonFiles = pickFiles('vendor', {
+    srcDir: '/sinonjs-built/pkg',
+    files: [
+      'sinon.js'
+    ],
+    destDir: '/assets/'
+  });
+
+var sinonChaiFiles = pickFiles('vendor', {
+    srcDir: '/sinon-chai/lib',
+    files: [
+      'sinon-chai.js'
+    ],
+    destDir: '/assets/'
+  });
+
+var mochaAdapter = pickFiles('vendor', {
+    srcDir: '/ember-mocha-adapter',
+    files: [
+      'adapter-qunit.js'
+    ],
+    destDir: '/assets/'
+  });
+
+var testTrees = mergeTrees([mochaFiles, mochaAdapter, chaiFiles, sinonFiles, sinonChaiFiles], {
+     overwrite: true
+});
+
+var fullTree = mergeTrees([app.toTree(), testTrees]);
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
